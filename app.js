cee6a18a73201e367f4639f190bd2713e5b34346
bicky <bickybong@gmail.com>
2022-08-16 11:08:38 +0800

    updated apps

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static("public"));
 //Listening on port 3000 and if it goes well then logging a message saying that the server is running
 app.listen(process.env.PORT||3000,function () {
  console.log("Server is running at port 3000");
-});
+}); 
 //Sending the signup.html file to the browser as soon as a request is made on localhost:3000
 app.get("/", function (req, res) {
  res.sendFile(__dirname + "/signup.html");
